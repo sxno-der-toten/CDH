369540b0b7872fd6b1924e0a5a313c2eab8508aa
JamyLaBiquette <melvin.jacques@coda-student.school>
2023-12-04 13:59:10 +0100

    tab modif

--- tableau.js
+++ tableau.js
@@ -9,14 +9,14 @@ function updatePosition() {
     cell.classList.remove('highlight');
   });
   const currentCell = document.querySelector(`#gameBoard tr:nth-child(${row + 1}) td:nth-child(${col + 1})`);
-  currentCell.classList.add('highlight');
+  currentCell.classList.add('highlight'); //highlight = personnage et faut mettre l'animation ici
 }
 
 // Gestionnaire d'événement pour détecter les touches du clavier
 document.addEventListener('keydown', function(event) {
   const key = event.key;
   const numRows = 6;
-  const numCols = 4;
+  const numCols = 5;
 
   if (key === 'ArrowUp' && row > 0) {
     row--;
@@ -30,6 +30,20 @@ document.addEventListener('keydown', function(event) {
 
   updatePosition();
 });
-
+function scrollTab(TableId, rowException){
+  let table = document.getElementById("gameBoard")
+  if (table) {
+    let tbody = table.getElementsByTagName('tbody')[0];
+    if (tbody) {
+      let rows = tbody.getElementsByTagName('tr');
+      for (let i = 0; i < rows.length; i++) {
+        if (i !== rowIndexToExclude) {
+          // Faites quelque chose avec la ligne, par exemple, ajoutez une classe
+          rows[i].classList.add('selected-row');
+        }
+      }
+    }
+  }
+}
 // Initialiser la position du rond
 updatePosition();
