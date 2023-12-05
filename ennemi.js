3dd244b77157d9e34d70abc25a5350568e46781e
sano <fawez.azzouk@coda-student.school>
2023-12-05 09:39:01 +0100

    Modification du projet : Fichiers remplacés
     	modified:   ennemi.js
     	modified:   gamejam.css
     	modified:   gamejam.html
     	new file:   perso.png
     	new file:   perso2.png
     	new file:   scaraber.png
     	modified:   tableau.js
     	new file:   tronc.png

--- ennemi.js
+++ ennemi.js
@@ -1,34 +1,45 @@
-function generateobstacle(){
+function generateObstacle() {
     let obstacle = [];
-    let i = 0;
-    while (i !==2) {
-        let nombreAleatoire = Math.floor(Math.random() * 5) + 1;
-        obstacle.push(nombreAleatoire) ;
-        while (obstacle[i] === obstacle[i - 1]){
-            let nombreAleatoire = Math.floor(Math.random() * 5) + 1;
-            let dernierElementSupprime = obstacle.pop();
-            obstacle.push(nombreAleatoire);
-
+  
+    while (obstacle.length !== 2) {
+      let nombreAleatoire = Math.floor(Math.random() * 5);
+      obstacle.push(nombreAleatoire);
+  
+      if (obstacle[0] === obstacle[1]) {
+        while (obstacle[0] === obstacle[1]) {
+          obstacle.pop();
+          let nouveauNombre = Math.floor(Math.random() * 5);
+          obstacle.push(nouveauNombre);
         }
+      }
     }
-
+  
     return obstacle;
-      
-} 
-
-const obstaclesFirstRow = generateobstacle();
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    
+  }
+  
+  function updateObstacles() {
+    const obstaclesFirstRow = generateObstacle();
     var firstRow = document.getElementById('gameBoard').getElementsByTagName('tr')[0];
-
     var cells = firstRow.getElementsByTagName('td');
-
-    for (let i = 0; i < obstaclesFirstRow.length; i++) {
-        elementsExtraits.push(obstaclesFirstRow[i]);
+  
+    for (let cell of cells) {
+      cell.innerHTML = ''; 
     }
-
-   
-});
+  
+    for (let i = 0; i < obstaclesFirstRow.length; i++) {
+     
+      let obstacleImage = document.createElement('img');
+      if (i === 0) {
+        obstacleImage.src = 'scaraber.png'; 
+      } else {
+        obstacleImage.src = 'perso.png'; 
+      }
+      obstacleImage.classList.add('obstacle');
+  
+      
+      cells[obstaclesFirstRow[i]].appendChild(obstacleImage);
+    }
+  }
+  
+  setInterval(updateObstacles, 2000);
+ 
--- tableau.js
+++ tableau.js
@@ -1,6 +1,6 @@
 // Coordonnées du rond
-let row = 0;
-let col = 0;
+let row = 3;
+let col = 2;
 
 // Fonction pour mettre à jour la position du rond dans le tableau
 function updatePosition() {
@@ -9,11 +9,20 @@ function updatePosition() {
     cell.classList.remove('highlight');
   });
   const currentCell = document.querySelector(`#gameBoard tr:nth-child(${row + 1}) td:nth-child(${col + 1})`);
-  currentCell.classList.add('highlight'); //highlight = personnage et faut mettre l'animation ici
+  
+  currentCell.classList.add('highlight');
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    updatePosition();
+  });
+  
 
 // Gestionnaire d'événement pour détecter les touches du clavier
 document.addEventListener('keydown', function(event) {
+    
   const key = event.key;
   const numRows = 6;
   const numCols = 5;
@@ -30,20 +39,7 @@ document.addEventListener('keydown', function(event) {
 
   updatePosition();
 });
-function scrollTab(TableId, rowException){
-  let table = document.getElementById("gameBoard")
-  if (table) {
-    let tbody = table.getElementsByTagName('tbody')[0];
-    if (tbody) {
-      let rows = tbody.getElementsByTagName('tr');
-      for (let i = 0; i < rows.length; i++) {
-        if (i !== rowIndexToExclude) {
-          // Faites quelque chose avec la ligne, par exemple, ajoutez une classe
-          rows[i].classList.add('selected-row');
-        }
-      }
-    }
-  }
-}
-// Initialiser la position du rond
-updatePosition();
+
+
+
+
