bd98af8f0165af4a170fd9300ab6d97649e46886
sano <fawez.azzouk@coda-student.school>
2023-12-05 11:34:23 +0100

    Commits :
    new file:   button_jouer.png
    modified:   gamejam.html
      	new file:   metre.js
      	new file:   perso3.png
      	new file:   perso4.png

---
+++ metre.js
@@ -0,0 +1,7 @@
+document.addEventListener('keydown', function(event) {
+    if(event.key === 'ArrowUp') {
+        // Action à effectuer lorsque la touche 'haut' est pressée
+        console.log("La touche 'haut' a été pressée !");
+        // Vous pouvez ajouter ici le code pour effectuer une action spécifique
+    }
+});
