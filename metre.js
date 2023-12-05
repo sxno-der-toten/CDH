b4865e1dfe835f7b0d94dabddefa0dd577c3de24
sano <fawez.azzouk@coda-student.school>
2023-12-05 15:27:50 +0100

    modified:   gamejam.html new file:   images/demon_fruit.png deleted:    metre.js

--- metre.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if(event.key === 'ArrowUp') {
-        // Action à effectuer lorsque la touche 'haut' est pressée
-        console.log("La touche 'haut' a été pressée !");
-        // Vous pouvez ajouter ici le code pour effectuer une action spécifique
-    }
-});
